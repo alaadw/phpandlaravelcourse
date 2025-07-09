b0dcb6e608694192e8dbd2666a45d6d82f298902
alaadw <alaa.dw@gmail.com>
2025-07-09 08:24:19 +0300

    Add files via upload

--- ourproject/inserting_products_from_outsideapi.php
+++ ourproject/inserting_products_from_outsideapi.php
@@ -14,56 +14,40 @@ function getCategoryName($categoryId) {
     }
     return 'Unknown Category';
 }
-// Fetch categories from database
-$addedCondition = ''; // Initialize additional condition for SQL query
-
-if(isset($_GET['category_id']) && !empty($_GET['category_id'])) {
-    $categoryId = (int)$_GET['category_id'];
-    $addedCondition = " AND products.category_id = $categoryId";
+/** fetch json from other website  */
+//https://fakestoreapi.com/products
+$json = file_get_contents('https://fakestoreapi.com/products');
+// Decode JSON data
+$products = json_decode($json, true);
+foreach($products as $product){
+    //echo $product['title']  . "<br>";
+    //price description  image
+   // echo $product['price'] . "<br>";
+   // echo $product['description'] . "<br>";
+   // echo '<img src="'.$product['image']. '"/>' . "<br>";
+    $title = $conn->real_escape_string($product['title']);
+    $desc = $conn->real_escape_string($product['description']);
+    $price = $conn->real_escape_string($product['price']);
+    $image = $conn->real_escape_string($product['image']);
+    
+    // Prepare and execute SQL statement to insert product
+   $sql  = "INSERT INTO products (name, category_id, description, price, featured, 
+   image)
+    VALUES ('{$title}', 3, '{$desc}', '{$price}', 0, '{$image}')";
+    if ($conn->query($sql) === TRUE) {
+        echo "New product created successfully<br>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 
 }
-//pager
-
-$limit = 10; // Number of records per page
-$page = isset($_GET['page']) ? (int)$_GET['page'] : 1; // Current page number
-$offset = ($page - 1) * $limit; // Calculate offset for SQL query
-$totalResult = $conn->query("SELECT COUNT(*) as total FROM products where name like '%" . $conn->real_escape_string($_GET['search'] ?? '') . "%'" . $addedCondition);
-$totalRow = $totalResult->fetch_assoc();    
-$total = $totalRow['total']; // Total number of records
-$totalPages = ceil($total / $limit); // Total number of pages
-
-
-?>
-<form action="" method="get">
-    <input type="search" name="search" placeholder="Search products..."
-     class="form-control" style="width: 300px; display: inline-block;">
-     <select class="" id="category_id" name="category_id" style="padding: 7px;
-    border-radius: 5px;
-    border: solid 1px #ccc;" required>
-            <option value="">Select Category</option>
-            <?php
-            // Fetch categories from database
-            $sql = "SELECT * FROM categories ORDER BY name ASC";
-            $result = $conn->query($sql);
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    echo "<option value='" . $row['id'] . "'>" . htmlspecialchars($row['name']) . "</option>";
-                }
-            }
-            ?>
-        </select>
-     <input type="submit" value="Search" class="btn btn-primary">
-    <a href="products_list.php" class="btn btn-secondary">Reset</a>
-</form>
-<?php
-$sql = "SELECT products.*,categories.name as catname FROM `products` 
-left JOIN categories on products.category_id = categories.id
-where products.name like '%" . $conn->real_escape_string($_GET['search'] ?? '') . "%'" . $addedCondition . "
-order by products.id asc
-limit $limit offset $offset
+die();
+// Fetch categories from database
+  
+$sql = "SELECT products.*,categories.name as catname FROM `products` left JOIN categories on products.category_id = categories.id;
 ";
-//die($sql);
-$result = $conn->query($sql);?>
+$result = $conn->query($sql);
+?>
 <form action="mass_delete_action_products.php" method="post">
 <div class="container mt-5">
     <h2><?php echo $title; ?></h2>
@@ -107,14 +91,6 @@ $result = $conn->query($sql);?>
     <input type="submit" name="mass_delete" value="Mass Delete" class="btn btn-danger">
                         </form>
 </div>
-<div class="text-center mt-4">
-    <?php if($page >1 ){ ?>
-        <a href="?page=<?php echo $page - 1; ?>" class="btn btn-primary">Previous</a>
-    <?php } ?>
-    <?php if($page < $totalPages ){ ?>
-        <a href="?page=<?php echo $page + 1; ?>" class="btn btn-primary">Next</a>
-    <?php } ?>
-</div>    
 <?php
 require 'includes/footer.php'; // Include footer file for closing HTML tags     
 ?>
