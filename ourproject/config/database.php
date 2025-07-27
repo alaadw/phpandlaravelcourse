6fdd8feff8cee081968439f485e4b3588e87af29
alaadw <alaa.dw@gmail.com>
2025-07-27 05:21:53 +0300

    Add files via upload

--- ourproject/config/database.php
+++ ourproject/config/database.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "firstproject");
+$conn = new mysqli("localhost", "root", "", "shop_db");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
